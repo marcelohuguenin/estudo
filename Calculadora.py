80792f424e0b2f3824748d8d4fad3784cee09122
Marcelo Huguenin <46941429+marcelohuguenin@users.noreply.github.com>
2024-06-12 21:20:28 -0300

    Create Calculadora.py
    Calculadora básica para estudo de código em questões matemáticas

---
+++ Calculadora.py
@@ -0,0 +1,22 @@
+a = int(input ("Digite o valor para A: "))
+while (a != 0):
+    b = int(input ("Digite o valor para B: "))
+    c = int(input ("1 para somar, 2 para subtrair, 3 para multipicar ou 4 para dividir: "))
+    if (c == 1):
+        resultado =int(a+b)
+        print ("a some de", a, "com", b, "é igual a:", resultado)
+    elif (c == 2):
+        resultado =int(a-b)
+        print ("a subitração de", a, "com", b, "é igual a:", resultado)
+    elif (c == 3):
+        resultado = a*b
+        print ("a multiplicação de", a, "com", b, "é igual a:", resultado)
+    elif (c == 4):
+        if (b == 0):
+            print ("Divisão por zero não permitida")
+        else:
+            resultado = float(a/b)
+            print ("a divisão de", a, "com", b, "é igual a:", resultado)
+    else:
+        print ("Opção Inválida.")
+    a = int(input ("Digite o valor para A: "))
